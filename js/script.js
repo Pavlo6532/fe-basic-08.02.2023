403dbe68a3c2b336977969dc8b0c9b9098f9973c
Pavlo6532 <lpn3005@gmail.com>
2023-04-11 22:19:21 +0300

    add about page

--- js/script.js
+++ js/script.js
@@ -4,3 +4,14 @@ $('.burger').click(function() {
     $(this).toggleClass('active')
     $('nav').slideToggle()
 });
+
+$(document).ready(function(){
+    $('#slider_about').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        dots: true,
+    });
+  });
+   
+ 
