45c50552754a8fbdeb242b96a7eba8fd6efe9e71
Pavlo6532 <lpn3005@gmail.com>
2023-04-13 20:23:55 +0300

    work slide about

--- js/script.js
+++ js/script.js
@@ -5,39 +5,22 @@ $('.burger').click(function() {
     $('nav').slideToggle()
 });
 
-// $(document).ready(function(){
-//     $('#slider_about').slick({
-//         infinite: true,
-//         slidesToShow: 4,
-//         slidesToScroll: 4,
-//         dots: true,
-//     });
-//   });
-   
-//   $(document).ready(function(){
-//     $('.owl-carousel').owlCarousel({
-//         loop:true,
-//         margin:10,
-//         nav:true,
-//         responsive:{
-//             0:{
-//                 items:1
-//             },
-//             600:{
-//                 items:3
-//             },
-//             1000:{
-//                 items:4
-//             }
-//         }
-//     })
-//     });
-
 $(document).ready(function(){
     $('#owl').owlCarousel({
       loop: true,
       nav: false,
       margin: 40,
       items: 4,
+      responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:4
+        },
+        1000:{
+            items:4
+        }
+    }
     });
   });
