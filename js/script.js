14f4055996e3f2238f99cea33eb7654269885623
Pavlo6532 <lpn3005@gmail.com>
2023-04-18 21:14:23 +0300

    add modal window

--- js/script.js
+++ js/script.js
@@ -1,28 +1,39 @@
-console.log('Work!');
+console.log("Work!");
 
-$('.burger').click(function() {
-    $(this).toggleClass('active')
-    $('nav').slideToggle()
+$(".burger").click(function () {
+  $(this).toggleClass("active");
+  $("nav").slideToggle();
 });
 
-$(document).ready(function(){
-    $('#owl').owlCarousel({
-      loop: true,
-      nav: false,
-      margin: 40,
-      items: 4,
-      autoplay: true,
-      responsive:{
-        0:{
-            items:1,
-            dotsEach: 2,
-        },
-        600:{
-            items:4
-        },
-        1000:{
-            items:4
-        }
-    }
-    });
+$(document).ready(function () {
+  $("#owl").owlCarousel({
+    loop: true,
+    nav: false,
+    margin: 40,
+    items: 4,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+        dotsEach: 2,
+      },
+      600: {
+        items: 4,
+      },
+      1000: {
+        items: 4,
+      },
+    },
   });
+});
+$(".modal").click(function () {
+  $(".modal-wrapper").addClass("active");
+});
+$(".modal-window .close, .modal-wrapper").click(function (event) {
+  console.log(this);
+  console.log(event.target);
+  if (this === event.target) {
+    console.log("Our target");
+    $(".modal-wrapper").removeClass("active");
+  }
+});
